6a758b3d74d53c4769847ff3c4eaa657bf65e14d
Evan Caraway <evan.caraway@outlook.com>
2020-11-24 23:49:18 -0800

    Project Files

---
+++ homeWorkWeek2Problem3 average/homeWorkWeek2Problem3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homeWorkWeek2Problem3
+{                                                                           //Evan Caraway, Srinivas Reddy Doma, Kipack Jeong
+   
+    class Program
+    {
+        public static void WriteAverage(int num1, int num2)                 //method named WriteAverage for taking the average of two numbers.
+        {
+            Console.WriteLine($"The average of {num1} and {num2} = {(num1 + num2)/2.0}");
+        }
+        static void Main(string[] args)
+        {
+            WriteAverage(2, 7);                                             // calling the method WriteAverage from the main method.
+        }
+        
+    }
+}
